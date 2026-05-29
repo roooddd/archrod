22bfd7afcd7148e92b5ebc0506f83443773236b3
roooddd <rod.rodrigoo123@gmail.com>
2026-05-29 17:53:40 -0300

    update

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -26,18 +26,18 @@ local browser     = "brave"
 ---- AUTOSTART ----
 -------------------
 hl.on("hyprland.start", function()
+    hl.exec_cmd("/usr/lib/xdg-desktop-portal")
+    hl.exec_cmd("/usr/lib/xdg-desktop-portal-gtk")
+    hl.exec_cmd("/usr/lib/xdg-desktop-portal-hyprland")
     hl.exec_cmd("/usr/lib/gvfsd")
     hl.exec_cmd("/usr/lib/gvfsd-fuse")
     hl.exec_cmd("playerctld daemon")
     hl.exec_cmd("hypridle")
-    hl.exec_cmd("mako -c ~/.config/mako/config")
-    hl.exec_cmd("/usr/bin/lxqt-policykit-agent")
     hl.exec_cmd("dbus-update-activation-environment --systemd --all")
-    hl.exec_cmd("/usr/lib/xdg-desktop-portal")
-    hl.exec_cmd("/usr/lib/xdg-desktop-portal-gtk")
-    hl.exec_cmd("/usr/lib/xdg-desktop-portal-hyprland")
-    hl.exec_cmd("waybar")
+    hl.exec_cmd("/usr/bin/lxqt-policykit-agent")
     hl.exec_cmd("awww-daemon")
+    hl.exec_cmd("waybar")
+    hl.exec_cmd("mako -c ~/.config/mako/config")
 end)
 
 
